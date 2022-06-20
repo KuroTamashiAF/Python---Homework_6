6a06674c65e15102584159fd67428ddcd10eebbd
Alex <chugun777akk@mail.ru>
2022-06-20 09:00:37 +0700

    task 2 is done

---
+++ Rewrite.py
@@ -0,0 +1,3 @@
+def write_in_file(in_file):
+    with open('output.txt', 'w') as data:
+        data.write(in_file)
--- controll.py
+++ controll.py
@@ -1,12 +1,13 @@
 import view_data
 import encode_block
 import decode_data
-
+import Rewrite
 
 def compression():
     data = view_data.enter_from_user()
     encode_block.encode(data)
     view_data.view(encode_block.encode(data)+' Сжатые данные')
+    Rewrite.write_in_file(encode_block.encode(data))
     print('-'*30)
     recovery_data = encode_block.encode(data)
     decode_data.decode(recovery_data)
